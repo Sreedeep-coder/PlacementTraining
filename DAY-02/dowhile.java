c7b8bc35e23feace61c60912e5590899a810681b
Sreedeep-coder <sreedeepdeep343@gmail.com>
2025-06-01 20:53:29 +0530

    Initial commit

---
+++ DAY-02/dowhile.java
@@ -0,0 +1,11 @@
+public class dowhile {
+    public static void main(string[] args){
+        int = 1;
+        do{
+            i++;
+            System.out.println(i +"Sreedeep");
+
+        }while (i <=10);
+    }
+    
+}
